562a66e00d9d9edcf037d644fc7ebe992eb50f59
anuraj-ghawate <ghawateanuraj@gmail.com>
2026-05-31 23:14:36 +0530

    Programming Based On Numbers

---
+++ C_Programming/Practice/program16.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckEvenOdd(int iNo)
+{
+     if((iNo % 2) == 0)
+     {
+        return true;
+     }
+     else
+     {
+        return false;
+     }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter Number To Check Whether It Is Even Or Odd : ");
+    scanf("%d",&iValue);
+
+    bRet = CheckEvenOdd(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d Is Even\n",iValue);
+    }
+    else
+    {
+        printf("%d Is Odd\n",iValue);
+    }
+
+    return 0;
+}
