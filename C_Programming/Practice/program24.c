80569bb63d42926e761a4725752981e634788cd1
anuraj-ghawate <ghawateanuraj@gmail.com>
2026-05-31 23:15:49 +0530

    Logic building using c programming

---
+++ C_Programming/Practice/program24.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+  // Sequence
+  
+    printf("Jay Gansh..\n");
+    printf("Jay Gansh..\n");
+    printf("Jay Gansh..\n");
+    printf("Jay Gansh..\n");
+
+  return 0;
+}
---
+++ C_Programming/Practice/program25.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  int iCnt = 0;
+
+  // Iteration
+  
+  for(iCnt = 1; iCnt <= 4; iCnt++)
+  {
+    printf("Jay Gansh...\n");
+  }
+
+  return 0;
+}
