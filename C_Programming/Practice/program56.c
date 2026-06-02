c03d3c1b7f51f964f2a4e0db70f9c69e11d15b9e
anuraj-ghawate <ghawateanuraj@gmail.com>
2026-06-02 23:29:55 +0530

    programming based on numbers and digits

---
+++ C_Programming/Practice/program56.c
@@ -0,0 +1,28 @@
+// Input : 5
+// output : 1 2 4 
+
+#include<stdio.h>
+
+void Dsiplay(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt < iNo; iCnt+=1)
+    {
+        printf("%d\t",iCnt);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    Dsiplay(iValue);
+
+    return 0;
+}
