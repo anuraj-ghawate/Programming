462fa5b7874175d54a55c3551d84e51bf35f67f1
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-06-14 23:22:22 +0530

    programming based on pattern printing

---
+++ Java_Programming/Practice/program204.java
@@ -0,0 +1,49 @@
+import java.util.*;
+/*
+    iRow = 4
+    iCol = 4;
+
+    a b c d
+    a b c d
+    a b c d
+    a b c d
+*/
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+        char Ch = '\0';
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1, Ch = 'a'; j <= iCol; j++,Ch++)
+            {
+                System.out.print(Ch+"\t");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+class program204
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter Number Of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number Of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+        
+    }
+}
