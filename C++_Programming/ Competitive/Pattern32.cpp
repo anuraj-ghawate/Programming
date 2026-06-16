833e3a05eecd189c872810982c7269bdfadfb09a
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-06-17 02:06:36 +0530

    programming based on pattern printing

---
+++ C++_Programming/ Competitive/Pattern32.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+void Display(int iRow,int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        cout<<"Invalid Input\n";
+        return;
+    }
+
+    for(i = iRow; i >= 1; i--)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+           if(j == i)
+           {
+            cout<<"# ";
+           }
+           else
+           {
+            cout<<"* ";
+           }
+        }
+      
+        cout<<"\n";
+    }    
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout<<"Enter A Number Of Rows :\n";
+    cin>>iValue1;
+
+    cout<<"Enter A Number Of Columns :\n";
+    cin>>iValue2;
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
