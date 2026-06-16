ae788890db19e17fc28d901eaf987dcbf6737ddf
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-06-16 23:59:04 +0530

    pattern printing using c++

---
+++ C++_Programming/ Competitive/Pattern17.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void Display(int iRow,int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        if(i % 2 == 0)
+        {
+            for(j = 1; j <= iCol; j = j+2)
+            {
+                cout<<j<<" ";      
+            }
+        }
+        else
+        {
+            for(j = 2; j <= iCol; j = j+2)
+            {
+                cout<<j<<" ";     
+            }
+        }
+
+            cout<<"\n";
+    }    
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout<<"Enter A Number Of Rows :\n";
+    cin>>iValue1;
+
+    cout<<"Enter A Number Of Columns :\n";
+    cin>>iValue2;
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
