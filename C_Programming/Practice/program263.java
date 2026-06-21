2e6388198632ff654eea6f00c6d3a4a6e90c3759
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-06-21 23:19:17 +0530

    programming based on strings

---
+++ C_Programming/Practice/program263.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class program263
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        
+        String Arr = null;
+
+        System.out.println("Enter A String");
+        Arr = sobj.nextLine();
+
+        System.out.println("Length Of String Is : "+Arr.length());
+
+    }
+}
