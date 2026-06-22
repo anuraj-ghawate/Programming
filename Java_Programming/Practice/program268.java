b20c1127cc312028aa8531be63346ed7eb2db840
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-06-22 22:45:55 +0530

    problems on strings using java

---
+++ Java_Programming/Practice/program268.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class StringX
+{
+    public void Display(String str)
+    {
+        System.out.println("String is : "+str);
+    }
+
+}
+
+class program268
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        StringX strobj = new StringX();
+        
+        String str = null;
+
+        System.out.println("Enter String :");
+        str = sobj.nextLine();
+
+        strobj.Display(str);
+
+    }
+}
