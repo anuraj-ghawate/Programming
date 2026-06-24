f943e66da477e6371eda79a35024f4972a5d2c24
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-06-24 22:04:52 +0530

    programming based on strings

---
+++ Java_Programming/Practice/program276.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class program276
+{   
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        StringX strobj = new StringX(); //Error
+        int iRet = 0;
+        
+        String data = null;
+
+        System.out.println("Enter String :");
+        data = sobj.nextLine();
+
+        iRet = strobj.CountCapital(data);
+
+        System.out.println("Number Of Capital Characters is : "+iRet);
+
+        iRet = strobj.CountSmall(data);
+
+        System.out.println("Number Of Small Characters is : "+iRet);
+
+         iRet = strobj.CountDigitsl(data);
+
+        System.out.println("Number Of Digit Are : "+iRet);
+
+        iRet = strobj.CountSpace(data);
+
+        System.out.println("Number OF White Spaces Are : "+iRet);
+
+        iRet = strobj.CountSpecial(data);
+
+        System.out.println("Number OF Special Symbols  Are : "+iRet);
+
+    }
+}
