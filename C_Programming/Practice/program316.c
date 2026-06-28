37ff37c458c1ec9255634aa79dbcb2e82a62d21a
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-06-28 23:57:57 +0530

    programming based on bitwise operators

---
+++ C_Programming/Practice/program316.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+int main()
+{
+    UINT iNo = 0;
+    UINT iMask = 0x40;
+    UINT iAns = 0;
+
+    printf("Enter Number\n");
+    scanf("%d",&iNo);
+
+    iAns = iNo & iMask;
+
+    if(iAns == iMask)
+    {
+        printf("7th Bit Is On");
+    }
+    else
+    {
+        printf("7th Bit Is Off");
+    }
+    
+
+    return 0;
+}
