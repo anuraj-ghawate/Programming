4cffdef12184fdbe780a6ba616f6433e4c352635
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-07-10 00:08:15 +0530

    Singly Linear LinkedList Using C++ OOP Technique

---
+++ C++_Programming/Data_Structures/LinkedList/program408.cpp
@@ -0,0 +1,154 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+
+class SinglyLL
+{
+    private:
+        PNODE first;
+        int iCount;
+
+    public:
+        SinglyLL();
+        void Display();
+        int Count();
+        void InsertFirst(int iNO);
+        void InsertLast(int iNo);
+        void InsertAtPos(int iNo, int iPos);
+        void DeleteFirst();
+        void DeleteLast();
+        void DeleteAtPos(int iPos);
+
+};
+
+SinglyLL:: SinglyLL()
+{
+    this -> first = NULL;
+    this -> iCount = 0;
+}
+
+
+void SinglyLL :: Display()
+{
+    PNODE temp = NULL;
+    int i = 0;
+
+    for (i = 1, temp = this->first; i <= this->iCount; i++, temp = temp->next) 
+    {
+       cout<<"| "<<temp->data<<" | -> ";
+    }
+    
+    cout<<"NULL"<<endl;
+}
+
+
+int SinglyLL :: Count()
+{
+    return this ->iCount;
+}
+
+void SinglyLL :: InsertFirst(int iNO)
+{
+    PNODE newn = NULL;
+    newn = new NODE;
+
+    newn->data = iNO;
+    newn->next = NULL;
+
+    if(this->iCount == 0)
+    {
+        this->first = newn;
+    }
+    else
+    {
+        newn -> next = this->first;
+        this -> first = newn;
+    }
+
+    this->iCount++; //Important
+}
+
+
+void SinglyLL :: InsertLast(int iNo)
+{
+    PNODE temp = NULL;
+    PNODE newn = NULL;
+    newn = new NODE;
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(this->iCount == 0)
+    {
+        this->first = newn;
+    }
+    else
+    {
+        temp = this -> first;
+
+        while (temp -> next != NULL)
+        {
+           temp = temp->next;
+        }
+
+        temp->next = newn;
+    }
+
+    this->iCount++; //Important
+}
+
+
+void SinglyLL :: InsertAtPos(int iNo, int iPos)
+{}
+
+
+void SinglyLL :: DeleteFirst()
+{
+    
+}
+
+
+void SinglyLL :: DeleteLast()
+{}
+
+
+void SinglyLL :: DeleteAtPos(int iPos)
+{}
+
+
+
+int main()
+{
+    int iRet = 0;
+
+    SinglyLL sobj;
+
+    sobj.InsertFirst(51);
+    sobj.InsertFirst(21);
+    sobj.InsertFirst(11);
+
+    sobj.Display();
+    iRet = sobj.Count();
+    cout<<"Number Of Elements Are : "<<iRet<<endl;
+    
+
+    sobj.InsertLast(101);
+    sobj.InsertLast(111);
+    sobj.InsertLast(121);
+
+    sobj.Display();
+    iRet = sobj.Count();
+    cout<<"Number Of Elements Are : "<<iRet<<endl;
+
+
+    return 0;
+}
