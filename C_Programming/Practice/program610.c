be10362e06613ad03ec6c2be2ce3dd065ed67d08
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-07-31 23:54:05 +0530

    File handeling using system calls

---
+++ C_Programming/Practice/program610.c
@@ -0,0 +1,163 @@
+//////////////////////////////////////////////////////////////////
+//
+//  Header Files Inclusion
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdbool.h>
+
+
+//////////////////////////////////////////////////////////////////
+//
+// User Defined Macros
+//
+//////////////////////////////////////////////////////////////////
+
+#define MAXFILESIZE 50
+#define MAXOPENFILES 10
+
+#define READ 1
+#define WRITE 2
+#define EXECUTE 4
+
+#define START 0
+#define CURRENT 1
+#define END 2
+
+#define EXECUTE_SUCCESS 0
+
+#define REGULARFILE 1
+#define SPECIALFILE 2
+
+
+
+//////////////////////////////////////////////////////////////////
+//
+// User Defined Macros For Error Handling
+//
+//////////////////////////////////////////////////////////////////
+
+#define ERR_INVALID_PARAMETER -1
+
+#define ERR_NO_INODES -2
+
+#define ERR_FILE_ALREADY_EXIST -3
+#define ERR_FILE_NOT_EXIST -4
+
+#define ERR_PERMISSION_DENIED -5
+
+#define ERR_INSUFFICIENT_SPACE -6
+
+#define ERR_INSUFFICIENT_DATA -7
+
+#define ERR_MAX_FILES_OPEN -8
+
+
+//////////////////////////////////////////////////////////////////
+//
+//  Structure Name :    BootBlock
+//  Description :       It holds the information to
+//                      boot the operating system
+//
+//////////////////////////////////////////////////////////////////
+
+struct BootBlock
+{
+    char Information[100];
+};
+
+
+//////////////////////////////////////////////////////////////////
+//
+//  Structure Name :    SuperBlock
+//  Description :       It holds the information of
+//                      complete file system
+//
+//////////////////////////////////////////////////////////////////
+
+struct SuperBlock
+{
+    int TotalInodes;
+    int FreeInodes;
+};
+
+
+//////////////////////////////////////////////////////////////////
+//
+//  Structure Name :    Inode
+//  Description :       It holds the information of files
+//
+//////////////////////////////////////////////////////////////////
+
+#pragma pack(1)
+struct Inode
+{
+    char FileName[20];
+    int InodeNumber;
+    int FileSize;
+    int ActualFileSize;
+    int FileType;
+    int ReferenceCount;
+    int Permission;
+    char *Buffer;
+    struct Inode *next;
+};
+
+typedef struct Inode INODE;
+typedef struct Inode* PINODE;
+typedef struct Inode** PPINODE;
+
+
+//////////////////////////////////////////////////////////////////
+//
+//  Structure Name :    FileTable
+//  Description :       It holds the information of opned files
+//
+//////////////////////////////////////////////////////////////////
+
+#pragma pack(1)
+struct FileTable
+{
+    int ReadOffset;
+    int WriteOffset;
+    int Mode;
+    PINODE ptrinode;
+    
+};
+
+typedef struct FileTable FILETABLE;
+typedef struct FileTable* PFILETABLE;
+
+
+//////////////////////////////////////////////////////////////////
+//
+//  Structure Name :    UAREA
+//  Description :       It holds the information of process
+//
+//////////////////////////////////////////////////////////////////
+
+struct UAREA
+{
+    char ProcessName[20];
+    PFILETABLE UFDT[MAXOPENFILES];
+};
+
+
+
+//////////////////////////////////////////////////////////////////
+//
+//  Entry Point Function Of The CVFS Project
+//
+//////////////////////////////////////////////////////////////////
+
+int main()
+{
+
+
+    return 0;
+}
