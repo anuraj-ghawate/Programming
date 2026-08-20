459f4b31df4f9b947613e8f5154d963d0415111e
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-08-20 08:56:57 +0530

    Reading & Writing Data Into Files
    Reading and writing the data into files using FileInputStream and FileOutputStream

---
+++ Java_Programming/Practice/program684.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+class program684
+{
+    public static void main(String A[]) throws Exception
+    {
+       String FnameSrc = null;
+       String FnameDest = null;
+
+       int iRet = 0;
+       File fobjsrc = null;
+       File fobjdest = null;
+
+       FileInputStream fiobj = null;
+       FileOutputStream foobj = null;
+
+       Scanner sobj = new Scanner(System.in);
+    
+       System.out.println("Enter The Source File Name :");
+       FnameSrc = sobj.nextLine();
+
+       System.out.println("Enter The Destination File Name :");
+       FnameDest = sobj.nextLine();
+
+       fobjsrc = new File(FnameSrc);
+       fobjdest = new File(FnameDest);
+
+       fiobj = new FileInputStream(fobjsrc);
+
+       foobj = new FileOutputStream(fobjdest);
+
+       fobjdest.createNewFile();
+
+       byte Buffer[] = new byte[100];
+
+       if(fobjsrc.exists())
+       {
+          String str = null;
+
+         while ((iRet = fiobj.read(Buffer)) != -1) 
+         {
+            foobj.write(Buffer,0,iRet);
+         }
+
+         fiobj.close();
+         foobj.close();
+       }
+       else
+       {
+            System.out.println("There Is No Such File ");
+       }
+    }
+    
+}
